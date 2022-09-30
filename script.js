2ffde2959035100b7a3b5768f1041cfb2e9d07c9
Nikola Zivaljevic <johnny8bargain@gmail.com>
2022-09-30 13:32:28 +0200

    pusham

--- script.js
+++ script.js
@@ -4,24 +4,35 @@ window.addEventListener('load', () => {
     const todoWrapper = document.querySelector('.todo__wrapper');
     const form = document.querySelector('#form');
     const input = document.querySelector('.todo__wrapper--input');
+    let ul = document.getElementById('ul');
 
-    // let listItem = document.querySelector('.todo__list--item');
+
     const buttonSubmit = document.querySelector('.todo__wrapper--button');
 
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        let list = document.querySelector('.todo__list');
+
+        // CREATING LIST ITEM 
         const li = document.createElement('li');
+        ul.appendChild(li);
 
-
-        li.textContent = input.value;
-        const buttonRemove = document.createElement('button');
-        buttonRemove.classList.add('todo__list--button');
-        buttonRemove.textContent = 'DELETE';
-        list.appendChild(buttonRemove);
-        list.appendChild(li);
         li.classList.add('todo__list--item');
+        console.log(li);
+        li.textContent = input.value;
+
+        //EDIT/SAVE BUTTON
+        let buttonEdit = document.createElement('button');
+
+        //DELETE BUTTON
+        let buttonRemove = document.createElement('button');
+        buttonRemove.classList.add('todo-list__button-remove');
+        buttonRemove.textContent = 'REMOVE';
+
+
+        li.appendChild(buttonRemove);
+
+        // DELETE FUNCTIONALITY
         buttonRemove.addEventListener('click', (e) => {
             console.log("bla")
             li.classList.add('none');
